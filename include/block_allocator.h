4e18435f20c4ecb3ad9e625e5137191f8d9eb233
roemil <emil.rosenberg@hotmail.com>
2024-11-16 15:33:44 +0100

    Block allocator using std list

--- include/block_allocator.h
+++ include/block_allocator.h
@@ -1,24 +1,26 @@
 #pragma once
 
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <memory>
 #include <new>
+#include <list>
+#include <iostream>
+
 template<typename std::size_t BlockSize>
 class BlockAllocator{
     private:
-        template<typename T>
         struct Block{
-            constexpr Block(T&& val) : val_(std::move(val)) {}
-            T val_;
-            bool is_free_ {false};
+            constexpr Block() = default;
+            std::byte data_[BlockSize]{};
+            bool is_free_ {true};
         };
     public:
     constexpr explicit BlockAllocator(std::size_t num_blocks) : num_blocks_(num_blocks), offset_(0) {
-        start_ = new std::byte[num_blocks_ * BlockSize];
-        assert(start_);
-    }
-    ~BlockAllocator() {
-        delete[] start_;
+        for(int i = 0; i < num_blocks_; ++i){
+            list_.emplace_back(new Block{});
+        }
     }
 
     constexpr std::size_t get_max_storage() const {
@@ -26,24 +28,66 @@ class BlockAllocator{
     }
 
     template<typename T>
+    constexpr T* allocate(T& val) {
+        return allocate(std::forward<T>(val));
+    }
+    template<typename T>
     constexpr T* allocate(T&& val) {
-        auto* block =  new(start_+offset_) Block<T>{std::forward<T>(val)};
+        assert(sizeof(T) <= BlockSize);
+        auto* block = find_free_block();
         if(!block) {
+            std::cout << "Failed to find free block\n";
             return nullptr;
         }
-        offset_ += sizeof(Block<T>);
-        return &block->val_;
+        block->is_free_ = false;
+        auto* ptr = new(block->data_) T{val};
+        return reinterpret_cast<T*>(ptr);
     }
 
     template<typename T>
     constexpr void deallocate(T* ptr){
-        Block<T>* block = start_+ptr;
-        block->is_free_ = true;
+        if(!ptr){
+            return;
+        }
+        auto block = std::find_if(list_.begin(), list_.end(), [ptr](auto block){
+            if(block){
+                auto* block_ptr = reinterpret_cast<T*>(block->data_);
+                if(!block_ptr){
+                    return false;
+                }
+                return block_ptr == ptr;
+            }
+            return false;
+        });
+        if(block != list_.end()) {
+            (*block)->is_free_ = true;
+        }
+    }
+
+    constexpr std::size_t count_occupied_blocks() const {
+        std::size_t count{};
+        for(const auto block : list_){
+            if(block && !block->is_free_){
+                ++count;
+            }
+        }
+        return count;
+
     }
 
 
     private:
+        Block* find_free_block() const {
+            auto it = std::find_if(list_.cbegin(), list_.cend(), [](auto* block){
+                return block && block->is_free_;
+            });
+            if(it == list_.end()){
+                return nullptr;
+            }
+            return *it;
+        }
+
         std::size_t num_blocks_{};
-        std::byte* start_;
+        std::list<Block*> list_{};
         std::size_t offset_{};
 };
--- test/block_allocator_suite.cpp
+++ test/block_allocator_suite.cpp
@@ -12,8 +12,28 @@ TEST(BlockAllocator, Alloc){
     const auto my_int = alloc.allocate(5);
     EXPECT_TRUE(my_int);
     EXPECT_EQ(*my_int, 5);
+    EXPECT_EQ(alloc.count_occupied_blocks(), 1);
 }
 
-TEST(BlockAllocator, Leak){
-    int* ptr = new int(5);
+TEST(BlockAllocator, Free){
+    BlockAllocator<10> alloc{10};
+    const auto my_int = alloc.allocate(5);
+    EXPECT_TRUE(my_int);
+    EXPECT_EQ(*my_int, 5);
+    EXPECT_EQ(alloc.count_occupied_blocks(), 1);
+
+    alloc.deallocate(my_int);
+    EXPECT_EQ(alloc.count_occupied_blocks(), 0);
+}
+
+
+
+TEST(BlockAllocator, FillAlloc){
+    constexpr std::size_t size = 10;
+    BlockAllocator<10> alloc{size};
+    for(int i = 0; i < size; ++i){
+        const auto my_int = alloc.allocate(i);
+        EXPECT_TRUE(my_int);
+    }
+    EXPECT_EQ(alloc.count_occupied_blocks(), size);
 }
