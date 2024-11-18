7dfb99a2795f4949f2ca9450c8415b00354fa5d6
Emil Rosenberg <emil.rosenberg@acorntechnology.se>
2024-11-18 12:04:12 +0100

    Replace raw ptr with unique ptr

--- include/block_allocator.h
+++ include/block_allocator.h
@@ -8,43 +8,55 @@
 #include <list>
 #include <iostream>
 
-template<typename T>
-class BlockAllocator{
-    private:
-        struct Block{
-            constexpr Block() = default;
-            alignas (T) std::byte data_[sizeof(T)]{};
-            bool is_free_ {true};
-        };
-    public:
-    constexpr explicit BlockAllocator(std::size_t num_blocks) : num_blocks_(num_blocks), offset_(0) {
-        for(int i = 0; i < num_blocks_; ++i){
-            list_.emplace_back(new Block{});
+template <typename T>
+class BlockAllocator
+{
+private:
+    struct Block
+    {
+        constexpr Block() = default;
+        alignas(T) std::byte data_[sizeof(T)]{};
+        bool is_free_{true};
+    };
+
+public:
+    constexpr explicit BlockAllocator(std::size_t num_blocks) : num_blocks_(num_blocks), offset_(0)
+    {
+        for (int i = 0; i < num_blocks_; ++i)
+        {
+            list_.emplace_back(std::make_unique<Block>());
         }
     }
 
-    constexpr std::size_t get_max_storage() const {
-        return num_blocks_*sizeof(T);
+    constexpr std::size_t get_max_storage() const
+    {
+        return num_blocks_ * sizeof(T);
     }
 
-    constexpr T* allocate(T& val) {
+    constexpr T *allocate(T &val)
+    {
         return allocate(std::forward<T>(val));
     }
-    constexpr T* allocate(T&& val) {
-        auto* block = find_free_block();
-        if(!block) {
+    constexpr T *allocate(T &&val)
+    {
+        auto *block = find_free_block();
+        if (!block)
+        {
             std::cout << "Failed to find free block\n";
             return nullptr;
         }
         block->is_free_ = false;
-        return reinterpret_cast<T*>(block->data_);
+        return reinterpret_cast<T *>(block->data_);
     }
 
-    constexpr void deallocate(T* ptr){
-        if(!ptr){
+    constexpr void deallocate(T *ptr)
+    {
+        if (!ptr)
+        {
             return;
         }
-        auto block = std::find_if(list_.begin(), list_.end(), [ptr](auto block){
+        auto block = std::find_if(list_.begin(), list_.end(), [ptr](auto &block)
+                                  {
             if(block){
                 auto* block_ptr = reinterpret_cast<T*>(block->data_);
                 if(!block_ptr){
@@ -52,38 +64,40 @@ class BlockAllocator{
                 }
                 return block_ptr == ptr;
             }
-            return false;
-        });
-        if(block != list_.end()) {
+            return false; });
+        if (block != list_.end())
+        {
             (*block)->is_free_ = true;
-            auto* ptr = reinterpret_cast<T*>((*block)->data_);
+            auto *ptr = reinterpret_cast<T *>((*block)->data_);
         }
     }
 
-    constexpr std::size_t count_occupied_blocks() const {
+    constexpr std::size_t count_occupied_blocks() const
+    {
         std::size_t count{};
-        for(const auto block : list_){
-            if(block && !block->is_free_){
+        for (const auto &block : list_)
+        {
+            if (block && !block->is_free_)
+            {
                 ++count;
             }
         }
         return count;
-
     }
 
-
-    private:
-        Block* find_free_block() const {
-            auto it = std::find_if(list_.cbegin(), list_.cend(), [](auto* block){
-                return block && block->is_free_;
-            });
-            if(it == list_.end()){
-                return nullptr;
-            }
-            return *it;
+private:
+    Block *find_free_block() const
+    {
+        auto it = std::find_if(list_.cbegin(), list_.cend(), [](auto &block)
+                               { return block && block->is_free_; });
+        if (it == list_.end())
+        {
+            return nullptr;
         }
+        return (*it).get();
+    }
 
-        std::size_t num_blocks_{};
-        std::list<Block*> list_{};
-        std::size_t offset_{};
+    std::size_t num_blocks_{};
+    std::list<std::unique_ptr<Block>> list_{};
+    std::size_t offset_{};
 };
